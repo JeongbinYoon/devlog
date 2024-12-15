d3269709e10de94b414cacb4f748e85e7a29489d
JeongbinYoon <yon980824@gmail.com>
2024-12-16 00:09:57 +0900

    feat: 메뉴 이동 시 하이라이트 모션 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,15 @@
 'use client';
 import { Bars3Icon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import { SunIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Nav from '@/components/Nav';
 
 const Header = () => {
-  const pathName = usePathname();
-  console.log('pathName', pathName);
   return (
     <header>
       <div className='flex justify-between items-center max-w-3xl h-16 mx-auto'>
         <h1>LOGO</h1>
 
-        <nav className='hidden sm:block'>
-          <Link href='/'>Home</Link>
-          <Link href='/blog'>Blog</Link>
-          <Link href='/about'>About</Link>
-          <div></div>
-        </nav>
+        <Nav />
 
         <div className='flex gap-1'>
           <button className='flex justify-center items-center border rounded-full size-9'>
@@ -27,7 +19,7 @@ const Header = () => {
             {/* <MoonIcon className='size-5' /> */}
             <SunIcon className='size-5' />
           </button>
-          <button className='flex sm:hidden justify-center items-center border rounded-full size-9 '>
+          <button className='flex sm:hidden justify-center items-center border rounded-full size-9'>
             <Bars3Icon className='size-5' />
           </button>
         </div>
---
+++ components/Nav.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useRef, useState, useLayoutEffect } from 'react';
+const menuItems = [
+  { name: 'Home', path: '/' },
+  { name: 'Blog', path: '/blog' },
+  { name: 'About', path: '/about' },
+];
+const Nav = () => {
+  const pathName = usePathname();
+  const menuRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+  const [highlightStyle, setHighlightStyle] = useState({
+    width: '0px',
+    height: '0px',
+    transform: 'translateX(0px)',
+    display: 'none',
+  });
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  //  하이라이트 위치 계산
+  useLayoutEffect(() => {
+    const currentIdx = menuItems.findIndex((item) => item.path === pathName);
+    const currentMenu = menuRefs.current[currentIdx];
+
+    if (currentMenu) {
+      const { offsetWidth, offsetHeight, offsetLeft } = currentMenu;
+      setHighlightStyle({
+        width: `${offsetWidth}px`,
+        height: `${offsetHeight}px`,
+        transform: `translateX(${offsetLeft}px)`,
+        display: 'block',
+      });
+      setIsLoading(false);
+    }
+  }, [pathName]);
+  return (
+    <nav className='hidden sm:flex relative h-fit'>
+      {menuItems.map((menu, idx) => (
+        <Link
+          key={menu.path}
+          href={menu.path}
+          className={`px-4 py-2.5 transition-colors rounded-full duration-300 ${
+            menu.path === pathName
+              ? `text-white ${isLoading && 'bg-black'}` // 새로고침 시 하이라이트 위치 계산 전 임시 배경으로 깜빡임 해결
+              : 'text-black'
+          }`}
+          ref={(el) => {
+            menuRefs.current[idx] = el;
+          }}
+        >
+          {menu.name}
+        </Link>
+      ))}
+
+      {/* 메뉴 하이라이트 */}
+      <div
+        className={`absolute left-0 top-0 -z-10 bg-black rounded-full transition-all duration-300`}
+        style={highlightStyle}
+      ></div>
+    </nav>
+  );
+};
+
+export default Nav;
