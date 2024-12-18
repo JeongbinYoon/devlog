cf738d463878a81855f1d22d6b8758453b148238
JeongbinYoon <yon980824@gmail.com>
2024-12-19 02:03:39 +0900

    feat: 동적 라우트 페이지 정적 렌더링

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,24 @@
-import { getPostDetail } from '@/lib/posts';
+import { getPostDetail, getSortedPostsData } from '@/lib/posts';
 
 type Params = { params: Promise<{ slug: string }> };
 
+export const generateStaticParams = async () => {
+  const posts = getSortedPostsData();
+  return posts.map((post) => ({ slug: post.slug }));
+};
+
 const PostDetailPage = async ({ params }: Params) => {
   const { slug } = await params;
-  const { content = '' } = getPostDetail(slug) || {};
-  return <div>{content}</div>;
+  const { id = '', title = '', content = '' } = getPostDetail(slug) || {};
+
+  if (!id) return <div>글을 불러올 수 없습니다.</div>;
+
+  return (
+    <div>
+      <h2 className='text-4xl mb-7'>{title}</h2>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default PostDetailPage;
