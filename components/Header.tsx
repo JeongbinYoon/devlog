95714a0852180624a93d15ee3ac59ef10db613b4
JeongbinYoon <yon980824@gmail.com>
2024-12-22 16:30:28 +0900

    feat: header, sidebar 위치 계산

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import HeaderTools from '@/components/HeaderTools';
 import Nav from '@/components/Nav';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+
+    window.addEventListener('scroll', handleScroll);
+    return window.addEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header>
+    <header
+      className={`sticky top-0 z-40 backdrop-blur transition-colors duration-300 bg-white/70 dark:bg-transparent border-b ${
+        isScrolled ? 'border-gray-0' : 'border-transparent'
+      }`}
+    >
       <div className='flex justify-between items-center max-w-3xl h-16 mx-auto px-5 md:px-0'>
         <h1>
           <Link href={'/'}>LOGO</Link>
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,20 +6,31 @@ import Nav from '@/components/Nav';
 import { useState, useEffect } from 'react';
 
 const SideBar = () => {
+  const HEADER_HEIGHT = 64;
   const isSidebarOpen = useAtomValue(isSidebarOpenAtom);
   const [shouldRender, setShouldRender] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+  const [scrollValue, setScrollValue] = useState(0);
 
   useEffect(() => {
     if (isSidebarOpen) {
+      document.body.style.overflow = 'hidden'; // 스크롤 잠금
       setShouldRender(true);
       setTimeout(() => setIsAnimating(true), 0);
     } else {
       setIsAnimating(false);
+      document.body.style.overflow = ''; // 스크롤 복원
       const timer = setTimeout(() => setShouldRender(false), 300); // 애니메이션 이후 DOM 제거
       return () => clearTimeout(timer);
     }
   }, [isSidebarOpen]);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollValue(window.scrollY);
+
+    window.addEventListener('scroll', handleScroll);
+    return window.addEventListener('scroll', handleScroll);
+  }, []);
 
   if (!shouldRender) return null;
 
@@ -28,7 +39,10 @@ const SideBar = () => {
       className={`block md:hidden absolute w-full px-5 py-8 bg-stone-100 transition-transform duration-300 ${
         isAnimating ? 'translate-x-0' : 'translate-x-full'
       }`}
-      style={{ height: 'calc(100vh - 64px)' }}
+      style={{
+        height: `calc(100vh - ${HEADER_HEIGHT}px)`,
+        top: `${HEADER_HEIGHT + scrollValue}px`,
+      }}
     >
       <Nav />
     </div>
