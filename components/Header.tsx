0e3827173757bd3473dfaeff938e5927a8c5e552
JeongbinYoon <yon980824@gmail.com>
2025-01-09 10:00:55 +0900

    feat: 스크롤에 따른 헤더 노출 설정정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,48 @@
-import { HeaderTools, Nav, ProgressBar } from '@/components';
+'use client';
+
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { HeaderTools, Nav, ProgressBar } from '@/components';
 
 const Header = () => {
+  const HIDE_HEADER_SCROLL_THRESHOLD = 700;
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // 헤더 표시 여부
+      if (
+        currentScrollY > lastScrollY &&
+        currentScrollY > HIDE_HEADER_SCROLL_THRESHOLD
+      ) {
+        setShowHeader(false);
+      } else setLastScrollY(currentScrollY);
+
+      // ProgressBar 업데이트
+      const scrollableHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const progress = window.scrollY / scrollableHeight;
+      setProgress(Math.min(progress, 1));
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollY]);
+
+  useEffect(() => setProgress(0), [pathName]);
+
   return (
-    <header className={`sticky top-0 z-50 backdrop-blur-lg bg-white/70`}>
+    <header
+      className={`sticky top-0 z-50 backdrop-blur-lg bg-white/70 transition ${
+        showHeader ? 'translate-y-0' : '-translate-y-full'
+      }`}
+    >
       <div className='flex justify-between items-center max-w-3xl h-16 mx-auto px-5 md:px-0'>
         <h1>
           <Link href={'/'}>LOGO</Link>
@@ -14,7 +53,7 @@ const Header = () => {
         </div>
         <HeaderTools />
       </div>
-      <ProgressBar />
+      <ProgressBar progress={progress} />
     </header>
   );
 };
