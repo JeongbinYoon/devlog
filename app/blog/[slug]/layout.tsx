ad031c151737cf3dcfad91bbfeba9a554a74dfa4
JeongbinYoon <yon980824@gmail.com>
2025-01-11 22:44:31 +0900

    fix: 좋아요 타입 에러 수정

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,15 +1,23 @@
 import { Likes } from '@/components';
+import { getPostDetailBySlug } from '@/lib/posts';
 
-export default function BlogLayout({
+export default async function BlogLayout({
   children,
+  params: paramsPromise,
 }: {
   children: React.ReactNode;
+  params: Promise<{ slug: string }>;
 }) {
+  const params = await paramsPromise;
+  const { slug } = params;
+  const { id = '' } =
+    (await getPostDetailBySlug(decodeURIComponent(slug))) || {};
+
   return (
     <div>
       {children}
       <aside className='fixed top-1/2 right-0 -translate-y-12 w-[calc((100vw-768px)/2)] lg:block hidden'>
-        <Likes />
+        <Likes postId={id} />
       </aside>
     </div>
   );
