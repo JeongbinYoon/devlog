38aee4a78003b9bb3ada7c0d178a2f99be32c346
JeongbinYoon <yon980824@gmail.com>
2025-01-16 01:55:20 +0900

    feat: 방명록 키보드 생성

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,32 +1,29 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Text } from '@react-three/drei';
 import * as THREE from 'three';
+import { keyboardData } from '@/app/constants';
+import { KeyBoardData, KeycapProps, WallProps } from '@/app/types/blog';
 
-interface WallProps {
-  position: [number, number, number];
-  rotation?: [number, number, number]; // rotation을 선택적으로 추가
-}
-
-function Wall({ position, rotation }: WallProps) {
+const Wall = ({ position, rotation }: WallProps) => {
   return (
     <lineSegments position={position} rotation={rotation}>
       <edgesGeometry args={[new THREE.BoxGeometry(10, 8, 0.3)]} />
       <lineBasicMaterial color='gray' />
     </lineSegments>
   );
-}
+};
 
-function Floor() {
+const Floor = () => {
   return (
     <lineSegments rotation={[-Math.PI / 2, 0, 0]} position={[0, -2.5, 0]}>
       <edgesGeometry args={[new THREE.BoxGeometry(10, 10, 0.3)]} />
       <lineBasicMaterial color='gray' />
     </lineSegments>
   );
-}
+};
 
-function Desk() {
+const Desk = () => {
   const deskTopPosition: [number, number, number] = [0, 0.4, 0]; // 책상 판 위치
   const leg1Height = 2.5; // 다리 높이
   const leg2Width = 2.5; // 다리 받침 길이
@@ -78,8 +75,8 @@ function Desk() {
       ))}
     </>
   );
-}
-function Monitor1() {
+};
+const Monitor1 = () => {
   const monitorPosition: [number, number, number] = [-2.35, 2.5, -0.5]; // 모니터 위치
   const monitorWidth = 3;
   const monitorHeigtht = 1.6875;
@@ -96,8 +93,8 @@ function Monitor1() {
       </mesh>
     </>
   );
-}
-function Monitor2() {
+};
+const Monitor2 = () => {
   const monitorPosition: [number, number, number] = [0, 2.5, -1]; // 모니터 위치
   const monitorWidth = 3;
   const monitorHeigtht = 1.6875;
@@ -111,8 +108,8 @@ function Monitor2() {
       </mesh>
     </>
   );
-}
-function Monitor3() {
+};
+const Monitor3 = () => {
   const monitor1Position: [number, number, number] = [2.4, 2.2, 0 - 1]; // 모니터 위치
   const monitor2Position: [number, number, number] = [2.4, 1.5, -0.6]; // 모니터 위치
   const monitorWidth = 1.6;
@@ -131,9 +128,73 @@ function Monitor3() {
       </mesh>
     </>
   );
-}
+};
 
-function Scene() {
+const Keycap = ({ position, label, width }: KeycapProps) => {
+  return (
+    <group position={position}>
+      {/* 키캡 */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <boxGeometry args={[width, 0.6, 0.8]} />
+        <meshStandardMaterial color='gray' wireframe />
+      </mesh>
+      {/* 키 라벨 */}
+      {label && (
+        <Text
+          position={[0, 0.2, 0]}
+          fontSize={0.2}
+          color='black'
+          anchorX='center'
+          anchorY='middle'
+        >
+          {label}
+        </Text>
+      )}
+    </group>
+  );
+};
+
+const Keyboard = () => {
+  // 키 위치 계산
+  const keyPositions: KeyBoardData[] = [];
+
+  // 배열을 순회하면서 각 키의 위치 계산
+  keyboardData.forEach((row, rowIndex) => {
+    let currentX = 0;
+    // currentX - (row.length * keyWidth) / 2,
+    row.forEach((key) => {
+      const keyWidth = key.width || 1; // 키에 맞는 너비 가져오기
+      const keyInfo: KeyBoardData = {
+        key,
+        position: [currentX + keyWidth / 2, -rowIndex, 0.3],
+      };
+      keyPositions.push(keyInfo); // 각 키의 위치 추가
+      currentX += keyWidth; // 다음 키의 X 위치를 갱신
+    });
+  });
+
+  return (
+    <group position={[0, 0, 0]}>
+      {/* 키캡 */}
+      {keyPositions.map(({ key, position }, index) => (
+        <Keycap
+          position={position}
+          key={key.name + index}
+          label={key.name}
+          width={key.width || 1} // 키 크기 지정
+        />
+      ))}
+
+      {/* 키보드 상판 */}
+      <mesh position={[8, -2, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <boxGeometry args={[16.4, 0.7, 5.2]} />
+        <meshStandardMaterial color='black' wireframe />
+      </mesh>
+    </group>
+  );
+};
+
+const Scene = () => {
   return (
     <Canvas>
       {/* 라이트 */}
@@ -145,6 +206,10 @@ function Scene() {
       <Floor />
       {/* Desk */}
       <Desk />
+      {/* Keyboard */}
+      <group position={[-8, 5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <Keyboard />
+      </group>
       {/* Monitor */}
       <Monitor1 />
       <Monitor2 />
@@ -153,7 +218,7 @@ function Scene() {
       <OrbitControls />
     </Canvas>
   );
-}
+};
 
 const GuestbookPage = () => {
   return (
--- app/types/blog.d.ts
+++ app/types/blog.d.ts
@@ -6,3 +6,24 @@ export interface Post {
   tags: string[];
   contentHtml: string;
 }
+
+interface WallProps {
+  position: [number, number, number];
+  rotation?: [number, number, number]; // rotation을 선택적으로 추가
+}
+
+interface KeyBoardKey {
+  name: string;
+  width?: number;
+}
+
+export interface KeyBoardData {
+  key: KeyBoardKey;
+  position: [number, number, number];
+}
+
+export interface KeycapProps {
+  position: [number, number, number];
+  label?: string;
+  width: number;
+}
