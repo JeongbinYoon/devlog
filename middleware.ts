7a4904945679a4db428b654a9f8f2c0a257a79e5
JeongbinYoon <yon980824@gmail.com>
2025-01-27 12:11:40 +0900

    test: 배포환경 쿠키 설정 테스트

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,6 @@ export async function middleware(request: NextRequest) {
   const lastVisitCookie = request.cookies.get(VISIT_KEY);
   const lastVisitDate = lastVisitCookie?.value;
   const now = new Date();
-  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
   const currentDate = now.toISOString().split('T')[0];
 
   const response = NextResponse.next();
@@ -14,8 +13,19 @@ export async function middleware(request: NextRequest) {
   const isHtmlRequest = request.headers.get('accept')?.includes('text/html');
   if (!isHtmlRequest) return response;
 
-  // 쿠키 정보로 해당 날짜 방문 여부 판단 후 업데이트
-  if (!lastVisitDate || currentDate > lastVisitDate) {
+  // 날짜 비교를 위해 Date 객체로 변환
+  const lastVisitDateTime = lastVisitDate ? new Date(lastVisitDate) : null;
+  const currentDateTime = new Date(currentDate);
+
+  console.log({
+    environment: process.env.NODE_ENV,
+    lastVisitDateTime,
+    currentDateTime,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  });
+
+  // 쿠키가 없거나, 마지막 방문일이 현재 날짜보다 이전인 경우
+  if (!lastVisitDateTime || lastVisitDateTime < currentDateTime) {
     try {
       response.cookies.set(VISIT_KEY, currentDate, {
         httpOnly: true,
@@ -33,8 +43,6 @@ export async function middleware(request: NextRequest) {
     } catch (error) {
       console.error('Route Handler 호출 오류:', error);
     }
-
-    return response;
   }
 
   return response;
