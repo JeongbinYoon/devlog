ebaebf6146bd7dac11f04a566b6613c6409bd361
JeongbinYoon <yon980824@gmail.com>
2025-03-04 02:22:08 +0900

    feat: 방명록 한글 입력 기능 추가

--- components/three/GuestbookInputs.tsx
+++ components/three/GuestbookInputs.tsx
@@ -1,17 +1,19 @@
 import { SubmitButton } from '@/components/three';
-import { useAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import {
   guestbookInputUserNameAtom,
   guestbookInputPasswordAtom,
   guestbookInputContentAtom,
 } from '@/app/atoms/appAtoms';
 import { Html } from '@react-three/drei';
+import { useEffect, useRef, useState } from 'react';
 
 const GuestbookInputs = ({ direction }: { direction: string }) => {
-  const [inputContent, setInputContent] = useAtom(guestbookInputContentAtom);
-  const [inputUserName, setInputUserName] = useAtom(guestbookInputUserNameAtom);
+  const setInputContent = useSetAtom(guestbookInputContentAtom);
+  const setInputUserName = useSetAtom(guestbookInputUserNameAtom);
   const [inputPassword, setInputPassword] = useAtom(guestbookInputPasswordAtom);
-
+  const [occludeKey, setOccludeKey] = useState(0); // occlude 초기화 키값
+  const [isInputFocus, setIsInputFocus] = useState(false);
   const commonStyles: React.CSSProperties = {
     border: 'none',
     borderRadius: '4px',
@@ -19,15 +21,36 @@ const GuestbookInputs = ({ direction }: { direction: string }) => {
     resize: 'none',
   };
 
+  useEffect(() => {
+    if (!isInputFocus) {
+      setOccludeKey((prev) => prev + 1); // input에서 focus out 하면 occlude 재적용
+    }
+  }, [isInputFocus]);
+
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputContentRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     // 가로 모니터 입력창
     direction === 'row' ? (
       <>
-        <Html position={[-0.93, -0.27, 0]} scale={0.25} transform>
+        <Html
+          position={[-9999, 0, 0]}
+          key={occludeKey}
+          occlude='blending'
+        ></Html>
+        <Html
+          position={[-0.93, -0.27, 0.04]}
+          scale={0.25}
+          transform
+          occlude='blending'
+        >
           <input
-            value={inputUserName}
+            ref={inputNameRef}
             placeholder='이름'
             onChange={(e) => setInputUserName(e.target.value)}
+            onFocus={() => setIsInputFocus(true)}
+            onBlur={() => setIsInputFocus(false)}
             style={{
               width: `135px`,
               height: '42px',
@@ -36,12 +59,19 @@ const GuestbookInputs = ({ direction }: { direction: string }) => {
             }}
           />
         </Html>
-        <Html position={[-0.93, -0.57, 0]} scale={0.25} transform>
+        <Html
+          position={[-0.93, -0.57, 0.04]}
+          scale={0.25}
+          transform
+          occlude='blending'
+        >
           <input
             type='password'
             value={inputPassword}
             placeholder='비밀번호'
             onChange={(e) => setInputPassword(e.target.value)}
+            onFocus={() => setIsInputFocus(true)}
+            onBlur={() => setIsInputFocus(false)}
             style={{
               width: `135px`,
               height: '42px',
@@ -50,11 +80,18 @@ const GuestbookInputs = ({ direction }: { direction: string }) => {
             }}
           />
         </Html>
-        <Html position={[0.45, -0.44, 0]} scale={0.25} transform>
+        <Html
+          position={[0.45, -0.44, 0.04]}
+          scale={0.25}
+          transform
+          occlude='blending'
+        >
           <textarea
-            value={inputContent}
+            ref={inputContentRef}
             placeholder='방명록을 남겨주세요'
             onChange={(e) => setInputContent(e.target.value)}
+            onFocus={() => setIsInputFocus(true)}
+            onBlur={() => setIsInputFocus(false)}
             style={{
               width: `295px`,
               height: '90px',
@@ -63,24 +100,26 @@ const GuestbookInputs = ({ direction }: { direction: string }) => {
             }}
           />
         </Html>
-        <SubmitButton />
-      </>
-    ) : (
-      // 세로 모니터 입력창
-      <Html position={[-1, -0.01, 0]} scale={0.25} transform>
-        <textarea
-          value={inputContent}
-          onChange={(e) => setInputContent(e.target.value)}
-          style={{
-            width: `240px`,
-            height: '110px',
-            padding: '8px',
-            transform: `rotate(90deg)`,
-            ...commonStyles,
-          }}
+        <SubmitButton
+          inputNameRef={inputNameRef}
+          inputContentRef={inputContentRef}
         />
-      </Html>
-    )
+      </>
+    ) : // 세로 모니터 입력창
+    // <Html position={[-1, -0.01, 0]} scale={0.25} transform>
+    //   <textarea
+    //     value={inputContent}
+    //     onChange={(e) => setInputContent(e.target.value)}
+    //     style={{
+    //       width: `240px`,
+    //       height: '110px',
+    //       padding: '8px',
+    //       transform: `rotate(90deg)`,
+    //       ...commonStyles,
+    //     }}
+    //   />
+    // </Html>
+    null
   );
 };
 
