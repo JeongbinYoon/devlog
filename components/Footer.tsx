051c07df875d026d7e1ec1ace09081043967c16d
JeongbinYoon <yon980824@gmail.com>
2025-11-14 10:47:07 +0900

    fix: 듀오링고 조회 revalidate 설정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,17 @@ import Duolingo from './Duolingo';
 export default async function Footer() {
   const fetchDuolingoData = async () => {
     const res = await fetch(
-      `https://www.duolingo.com/2017-06-30/users?username=${process.env.DUOLINGO_ID}`
+      `https://www.duolingo.com/2017-06-30/users?username=${process.env.DUOLINGO_ID}`,
+      {
+        next: { revalidate: 3600 },
+      }
     );
     const json = await res.json();
     return json?.users?.[0];
   };
 
   const duolingoUserData = await fetchDuolingoData();
-  console.log('duolingoUserData', duolingoUserData);
+
   return (
     <div className="bg-gray-200 mt-auto px-10 py-7">
       <div className="max-w-3xl mx-auto">
